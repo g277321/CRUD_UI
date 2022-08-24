c177ae10585c8a0fe44f1daea8b491b1a4be9c9e
PPG <g277321@gmail.com>
2022-08-24 20:40:39 +0200

    Initial Commit

---
+++ CRUD_UI/CRUD_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CRUD_UIApp.swift
+//  CRUD_UI
+//
+//  Created by PPG on 24.08.22.
+//
+
+import SwiftUI
+
+@main
+struct CRUD_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CRUD_UI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CRUD_UI
+//
+//  Created by PPG on 24.08.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
